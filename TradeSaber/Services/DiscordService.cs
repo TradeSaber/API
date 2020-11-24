883f428ed73c6cb52233a7dbd1243788bd792f04
Auros Nexus <aurosnex@gmail.com>
2020-11-24 03:27:36 -0500

    logged up discord service

--- TradeSaber/Services/DiscordService.cs
+++ TradeSaber/Services/DiscordService.cs
@@ -6,22 +6,26 @@ using System.Threading.Tasks;
 using System.Net.Http.Headers;
 using TradeSaber.Models.Discord;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace TradeSaber.Services
 {
     public class DiscordService
     {
         private readonly HttpClient _client;
+        private readonly ILogger<DiscordService> _logger;
         private readonly DiscordSettings _discordSettings;
     
-        public DiscordService(HttpClient client, DiscordSettings discordSettings)
+        public DiscordService(HttpClient client, ILogger<DiscordService> logger, DiscordSettings discordSettings)
         {
             _client = client;
+            _logger = logger;
             _discordSettings = discordSettings;
         }
 
         public async Task<string> GetAccessToken(string code)
         {
+            _logger.LogDebug("Fetching Access Token");
             Dictionary<string, string> parameters = new Dictionary<string, string>
             {
                 { "client_id", _discordSettings.ID },
@@ -34,22 +38,33 @@ namespace TradeSaber.Services
             HttpResponseMessage response = await _client.PostAsync(_discordSettings.URL + "/oauth2/token", content);
             if (response.IsSuccessStatusCode)
             {
+                _logger.LogDebug("Received Access Token");
                 Stream responseStream = await response.Content.ReadAsStreamAsync();
                 AccessTokenResponse accessTokenResponse = await JsonSerializer.DeserializeAsync<AccessTokenResponse>(responseStream);
                 return accessTokenResponse.AccessToken;
+            }
+            else
+            {
+                _logger.LogWarning("Could not get access token. {ReasonPhrase}", response.ReasonPhrase);
             }
             return null;
         }
 
         public async Task<DiscordUser> GetProfile(string accessToken)
         {
+            _logger.LogDebug("Getting active user profile.");
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
             HttpResponseMessage response = await _client.GetAsync(_discordSettings.URL + "/users/@me");
             if (response.IsSuccessStatusCode)
             {
                 string responseString = await response.Content.ReadAsStringAsync();
                 DiscordUser discordUser = JsonSerializer.Deserialize<DiscordUser>(responseString);
+                _logger.LogDebug("User Profile {Username}#{Discriminator} Found", discordUser.Username, discordUser.Discriminator);
                 return discordUser;
+            }
+            else
+            {
+                _logger.LogWarning("Could not get user profile. {ReasonPhrase}", response.ReasonPhrase);
             }
             return null;
         }
