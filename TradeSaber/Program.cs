cdf6b6cfbbd356e3e8f42da87ce4029613f5ef03
Auros Nexus <aurosnex@gmail.com>
2020-11-26 21:25:00 -0500

    Prettier Logging

--- TradeSaber/Program.cs
+++ TradeSaber/Program.cs
@@ -1,14 +1,16 @@
 using Serilog;
+using Serilog.Core;
 using Serilog.Events;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace TradeSaber
 {
     public class Program
     {
-        public const string LOG_OUTPUT_FORMAT = "[{Timestamp:HH:mm:ss} | {Level:u3}] {Message:lj}{NewLine}{Exception}";
-        public const string LOG_OUTPUT_FORMAT_FILE = "[{Timestamp:HH:mm:ss} | {Level:u3}] ({SourceContext}) {Message:lj}{NewLine}{Exception}";
+        public const string LOG_OUTPUT_FORMAT = "[{Timestamp:HH:mm:ss} | {Level:u3}] {LooseSource} {Message:lj}{NewLine}{Exception}";
 
         public static void Main(string[] args)
         {
@@ -16,10 +18,11 @@ namespace TradeSaber
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: LOG_OUTPUT_FORMAT)
+                .Enrich.With<LooseSourceEnricher>()
+                .WriteTo.Console(outputTemplate: LOG_OUTPUT_FORMAT, theme: AnsiConsoleTheme.Code)
                 .WriteTo.File("Logs/log-.txt",
                     rollingInterval: RollingInterval.Day,
-                    outputTemplate: LOG_OUTPUT_FORMAT_FILE,
+                    outputTemplate: LOG_OUTPUT_FORMAT,
                     rollOnFileSizeLimit: true)
                 .CreateLogger();
             CreateHostBuilder(args).Build().Run();
@@ -32,5 +35,30 @@ namespace TradeSaber
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public class LooseSourceEnricher : ILogEventEnricher
+        {
+            private readonly Dictionary<string, LogEventProperty> _propertyCache = new Dictionary<string, LogEventProperty>();
+            private static readonly string looseSourceName = "LooseSource";
+            private static readonly string sourceName = "SourceContext";
+
+            public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+            {
+                if (logEvent.Properties.TryGetValue(sourceName, out LogEventPropertyValue? value) && value != null)
+                {
+                    var subs = value.ToString().Split('.');
+                    if (subs.Length > 0)
+                    {
+                        var sub = $"[{subs[^1].Replace("\"", "")}]";
+                        if (!_propertyCache.TryGetValue(sub, out LogEventProperty? property))
+                        {
+                            property = propertyFactory.CreateProperty(looseSourceName, sub);
+                            _propertyCache.TryAdd(sub, property);
+                        }
+                        logEvent.AddPropertyIfAbsent(property);
+                    }
+                }
+            }
+        }
     }
 }
