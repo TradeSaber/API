4e35f7734714889df89360d476aa92c283a14bcd
Auros Nexus <aurosnex@gmail.com>
2021-07-26 09:36:19 -0400

    Added Serilog

--- TradeSaber/Program.cs
+++ TradeSaber/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace TradeSaber
 {
@@ -7,14 +9,24 @@ namespace TradeSaber
     {
         public static void Main(string[] args)
         {
+            Log.Logger = TradeSaberLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static ILogger TradeSaberLogger()
+        {
+            var config = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+            return config.CreateLogger();
+        }
     }
 }
