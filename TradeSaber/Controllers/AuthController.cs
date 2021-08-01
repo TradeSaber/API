109ba68d0a16e98c13666d416ae6620b442fd9a7
Auros Nexus <aurosnex@gmail.com>
2021-07-31 21:00:21 -0400

    Removed Weather

--- TradeSaber/Controllers/AuthController.cs
+++ TradeSaber/Controllers/AuthController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using TradeSaber.Models;
+using TradeSaber.Services;
 
 namespace TradeSaber.Controllers
 {
@@ -6,5 +10,32 @@ namespace TradeSaber.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly ILogger _logger;
+        private readonly SoriginAuthorizer _soriginAuthorizer;
+        private readonly TradeSaberAuthService _tradeSaberAuthService;
+
+
+        public AuthController(ILogger<AuthController> logger, SoriginAuthorizer soriginAuthorizer, TradeSaberAuthService tradeSaberAuthService)
+        {
+            _logger = logger;
+            _soriginAuthorizer = soriginAuthorizer;
+            _tradeSaberAuthService = tradeSaberAuthService;
+        }
+
+        public async Task<ActionResult> Login([FromBody] LoginBody body)
+        {
+            _logger.LogInformation("Authorizing via Sorigin.");
+            SoriginUser? soriginUser = await _soriginAuthorizer.Authorize(body.Token);
+            if (soriginUser is null)
+            {
+                return Unauthorized(Error.Create("Could not authorize through Sorigin."));
+            }
+            throw new System.NotImplementedException();
+        }
+
+        public class LoginBody
+        {
+            public string Token { get; set; } = null!;
+        }
     }
 }
